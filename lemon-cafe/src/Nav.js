14ba7feffbebf4feefb5175dd9e15935961c3f27
SvitlanaBahlai <conbka@gmail.com>
2024-09-26 11:15:06 +0300

    added some marking for reservation page

--- lemon-cafe/src/Nav.js
+++ lemon-cafe/src/Nav.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav>
+    <nav role="navigation">
       <ul>
          <li>
             <Link to="/">Home</Link>
--- lemon-cafe/src/ReservationsPage.js
+++ lemon-cafe/src/ReservationsPage.js
@@ -2,19 +2,47 @@
 
 import './App.css';
 import './Main.css'
+import './ReservationsPage.css'
 
 
 function ReservationsPage() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <h6>RESERVATIONS PAGE!!!
-          <br/>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</h6>
+    <div className="booking-container">
+      <div className='description-block'>
+        <h3>Book with us!</h3>
+        <p>Please complete the table reservation form.</p>
+      </div>
+      <div className='info-block'>
+        <div>
+          <label>Select GUEST*</label>
+          <input for='guest' id='guest' type='text' ></input>
         </div>
-        <div className="col-12 col-md-6">
-          <h6>RESERVATIONS PAGE!!!
-          <br/>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</h6>
+        <div>
+          <label>Select Date*</label>
+          <input for='date' id='date' type='text' ></input>
+        </div>
+        <div>
+          <label>Select HOUR*</label>
+          <input for='hour' id='hour' type='text' ></input>
+        </div>
+      </div>
+      <div className='seat-block'>
+        <label className='seat-label' >Seating area*</label>
+        <div className='seat-item'>
+          <label>Hall</label>
+          <input for='hall' id='hall' type="checkbox" />
+        </div>
+        <div className='seat-item'>
+          <label>Outside</label>
+          <input for='outside' id='outside' type="checkbox" />
+        </div>
+        <div className='seat-item'>
+          <label>Yard</label>
+          <input for='yard' id='yard' type="checkbox" />
+        </div>
+        <div className='seat-item'>
+          <label>Terrace</label>
+          <input for='terrace' id='terrace' type="checkbox" />
         </div>
       </div>
     </div>
