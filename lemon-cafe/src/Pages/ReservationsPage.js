c7e1941bee30f8a73b16e5912c01803972db2796
cvetok <conbka@gmail.com>
2024-09-27 20:19:51 +0300

    rename for to htmlFor in label

--- lemon-cafe/src/Pages/ReservationsPage.js
+++ lemon-cafe/src/Pages/ReservationsPage.js
@@ -10,9 +10,9 @@ function ReservationsPage() {
     <div className='booking-container'>
       <form className='booking-form'>
         <div className='info-block'>
-          <label for="res-date">Choose date</label>
+          <label htmlFor="res-date">Choose date</label>
           <input type="date" id="res-date"></input>
-          <label for="res-time">Choose time</label>
+          <label htmlFor="res-time">Choose time</label>
           <select id="res-time ">
             <option>17:00</option>
             <option>18:00</option>
@@ -21,10 +21,10 @@ function ReservationsPage() {
             <option>21:00</option>
             <option>22:00</option>
           </select>
-          <label for="guests">Number of guests</label>
+          <label htmlFor="guests">Number of guests</label>
           <input type="number" placeholder="1" min="1" max="10" id="guests"></input>
         </div>
-        <label for="occasion">Occasion</label>
+        <label htmlFor="occasion">Occasion</label>
         <div className='seat-block'>
           <select id="occasion">
             <option>Birthday</option>
