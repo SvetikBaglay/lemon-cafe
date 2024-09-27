bd6591827e630aed28c9b7e5efc043bf6ffdd0bc
cvetok <conbka@gmail.com>
2024-09-27 16:47:53 +0300

    adde style for footer component

--- lemon-cafe/src/AboutSection.js
+++ lemon-cafe/src/AboutSection.js
@@ -6,13 +6,6 @@ function AboutSection() {
   return (
     <section className='about-section'>
       <AboutPage />
-      {/* <div className="flex-container hero-container">
-        <div className="hero-description">
-          <h1> AboutSection </h1>
-          <h5>Chicago</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>  
-      </div> */}
     </section>
    
   );
