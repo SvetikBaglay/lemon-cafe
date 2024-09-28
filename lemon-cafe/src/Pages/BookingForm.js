fd82e330311a4c421e72663379c790af7743a15d
cvetok <conbka@gmail.com>
2024-09-28 14:42:24 +0300

    added occasion mat select

--- lemon-cafe/src/Pages/BookingForm.js
+++ lemon-cafe/src/Pages/BookingForm.js
@@ -3,27 +3,34 @@
 import '../App.css';
 import './Main.css'
 import './BookingForm.css'
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 
 function BookingForm({ availableTimes }) {
   const [resDate, setResDate] = useState('');
   const [resTime, setResTime] = useState('17:00');
   const [numberGuests, setNumberGuests] = useState(1);
+  const [occasion, setOccasion] = useState('Birthday');
+
 
   function handleChangeResDate(e) {
     setResDate(e.target.value);
-    console.log('Selected date:', e.target.value);
+    console.log('selected date:', e.target.value);
   }
 
   function handleChangeNumberGuests(e) {
     setNumberGuests(e.target.value);
-    console.log('Number of guests:', e.target.value);
+    console.log('number  guests:', e.target.value);
   }
 
   function handleSelect(e) {
     setResTime(e.target.value);
-    console.log('Selected time:', e.target.value);
+    console.log('selected time:', e.target.value);
+  }
+
+  function handleOccasion(e) {
+    setOccasion(e.target.value)
+    console.groupCollapsed('selected Occasion', e.target.value)
   }
 
   function handleSubmit(e) {
@@ -33,6 +40,7 @@ function BookingForm({ availableTimes }) {
       date: resDate,
       time: resTime,
       guests: numberGuests,
+      occasion: occasion,
     };
     console.log('formData', formData);
   }
@@ -41,29 +49,29 @@ function BookingForm({ availableTimes }) {
     <div className='booking-container'>
       <form onSubmit={handleSubmit} className='booking-form'>
         <div className='info-block'>
-          <label htmlFor="res-date">Choose date</label>
-          <input onChange={handleChangeResDate} value={resDate} type="date" name='date' id="date" />
+          <label htmlFor='res-date'>Choose date</label>
+          <input onChange={handleChangeResDate} value={resDate} type='date' name='date' id='date' />
           
-          <label htmlFor="res-time">Choose time</label>
-          <select value={resTime} onChange={handleSelect} id="resTime" name="resTime">
+          <label htmlFor='res-time'>Choose time</label>
+          <select value={resTime} onChange={handleSelect} id='resTime' name='resTime'>
             {availableTimes.map((time) => (
               <option key={time} value={time}>{time}</option>
             ))}
           </select>
           
-          <label htmlFor="guests">Number of guests</label>
-          <input onChange={handleChangeNumberGuests} value={numberGuests} type="number" name='numberGuests' id='numberGuests' min="1" max="10" />
+          <label htmlFor='guests'>Number of guests</label>
+          <input onChange={handleChangeNumberGuests} value={numberGuests} type='number' name='numberGuests' id='numberGuests' min='1' max='10' />
         </div>
         
-        <label htmlFor="occasion">Occasion</label>
+        <label htmlFor='occasion'>Occasion</label>
         <div className='seat-block'>
-          <select id="occasion">
-            <option>Birthday</option>
-            <option>Anniversary</option>
+          <select value={occasion} onChange={handleOccasion} id='occasion' name='occasion'>
+            <option value='Birthday'>Birthday</option>
+            <option value='Anniversary'>Anniversary</option>
           </select>
           
           <input className='button button-secondary' name='reservations' id='reservations' 
-            type="submit" value="Make Your reservation" />
+            type='submit' value='Make Your reservation' />
         </div>
       </form>
     </div>
